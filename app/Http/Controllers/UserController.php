681992f5b8d0953ce42f145e6236802ddba85df6
echo <echolin07@gmail.com>
2016-09-10 18:01:09 +0800

    test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,28 +13,29 @@ class UserController extends Controller
 {
     public function index($uid = false)
     {
-        $user = user($uid);
-        $uid = $user->id;
+        return response()->json('success into user index');
+     //    $user = user($uid);
+     //    $uid = $user->id;
 
-    	if (!$user['follows'] = Cache::get(USER_FOLLOWS_COUNT . $uid)) {
-            if (!$user['follows'] = Redis::scard(USER_FOLLOWS_SET . $uid)) {
-    		  $user['follows'] = DB::connection('follows')->table(getFollowsTable($uid))->where('uid', $uid)->count();
-            }
-    		Cache::forever(USER_FOLLOWS_COUNT . $uid, $user['follows']);
-    	}
+    	// if (!$user['follows'] = Cache::get(USER_FOLLOWS_COUNT . $uid)) {
+     //        if (!$user['follows'] = Redis::scard(USER_FOLLOWS_SET . $uid)) {
+    	// 	  $user['follows'] = DB::connection('follows')->table(getFollowsTable($uid))->where('uid', $uid)->count();
+     //        }
+    	// 	Cache::forever(USER_FOLLOWS_COUNT . $uid, $user['follows']);
+    	// }
 
-    	if (!$user['follows_me'] = Cache::get(USER_FOLLOWS_ME_COUNT . $uid)) {
-            if (!$user['follows_me'] = Redis::scard(USER_FOLLOWS_ME_SET . $uid)) {
-    		  $user['follows_me'] = DB::connection('follows')->table(getFollowsMeTable($uid))->where('follow_uid', $uid)->count();
-            }
-    		Cache::forever(USER_FOLLOWS_ME_COUNT . $uid, $user['follows_me']);
-    	}
+    	// if (!$user['follows_me'] = Cache::get(USER_FOLLOWS_ME_COUNT . $uid)) {
+     //        if (!$user['follows_me'] = Redis::scard(USER_FOLLOWS_ME_SET . $uid)) {
+    	// 	  $user['follows_me'] = DB::connection('follows')->table(getFollowsMeTable($uid))->where('follow_uid', $uid)->count();
+     //        }
+    	// 	Cache::forever(USER_FOLLOWS_ME_COUNT . $uid, $user['follows_me']);
+    	// }
 
-    	if (!$user['feeds'] = Cache::get(USER_FEEDS_COUNT . $uid)) {
-    		$user['feeds'] = DB::connection('feeds')->table(getFeedsIndexTable($uid))->where('uid', $uid)->where('status',STATUS_CHECKED)->count();
-    		Cache::forever(USER_FEEDS_COUNT . $uid, $user['feeds']);
-    	}
+    	// if (!$user['feeds'] = Cache::get(USER_FEEDS_COUNT . $uid)) {
+    	// 	$user['feeds'] = DB::connection('feeds')->table(getFeedsIndexTable($uid))->where('uid', $uid)->where('status',STATUS_CHECKED)->count();
+    	// 	Cache::forever(USER_FEEDS_COUNT . $uid, $user['feeds']);
+    	// }
 
-        return response()->json($user);
+     //    return response()->json($user);
     }
 }

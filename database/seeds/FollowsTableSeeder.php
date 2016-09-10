aa3b2bd405d4957bfbe028f8776315c01898ae5b
echo <echolin07@gmail.com>
2016-09-10 13:55:47 +0800

    关注表初始化

--- database/seeds/FollowsTableSeeder.php
+++ database/seeds/FollowsTableSeeder.php
@@ -13,16 +13,16 @@ class FollowsTableSeeder extends Seeder
     {
         $i = 0;
     	while ($i<1000) {
-    		// $uid = rand(1,2000);
-    		// $follow_uid = rand(1,2000);
-    		// while ($uid == $follow_uid) {
-    		// 	$follow_uid = rand(1,2000);
-    		// }
-            $uid = rand(1,2000);
-            $follow_uid = 1024;
-            while ($uid == $follow_uid) {
-                $uid = rand(1,2000);
-            } 
+    		$uid = rand(1,2000);
+    		$follow_uid = rand(1,2000);
+    		while ($uid == $follow_uid) {
+    			$follow_uid = rand(1,2000);
+    		}
+            // $uid = rand(1,2000);
+            // $follow_uid = 1024;
+            // while ($uid == $follow_uid) {
+            //     $uid = rand(1,2000);
+            // } 
     		$follows = 'follows_'.sprintf('%04d', $uid%1024);
     		$follows_me = 'follows_me_'.sprintf('%04d', $follow_uid%1024);
     		if(DB::connection('follows')->table($follows)->where('uid', '=', $uid)->where('follow_uid', '=', $follow_uid)->count()){

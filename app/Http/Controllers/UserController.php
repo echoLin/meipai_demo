2e14265455c31a600920b9632811b3b21a7cad86
echo <echolin07@gmail.com>
2016-09-13 17:12:27 +0800

    redis bug

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use Cache;
-use Redis;
 use DB;
 use App\User;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redis as Redis;
 use App\Http\Requests;
 
 class UserController extends Controller

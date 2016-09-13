1cd5c3f576cc7bbb8b03b3c04fb91da6e11273ef
echoLin <409086340@qq.com>
2016-09-13 23:59:24 +0800

    test

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis as Redis;
 use App\Jobs\PostFollow;
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class FollowController extends Controller
 {
@@ -33,26 +34,26 @@ class FollowController extends Controller
             return response()->json('您已关注' . MAX_FOLLOWS_COUNT . '人，不可再关注他人');
          }
 
-         //$this->dispatch(new PostFollow($uid, $follow_uid));
+         $this->dispatch(new PostFollow($uid, $follow_uid));
 
-         $follows_table = getFollowsTable($uid);
-         $follows_me_table = getFollowsMeTable($follow_uid);
+         // $follows_table = getFollowsTable($uid);
+         // $follows_me_table = getFollowsMeTable($follow_uid);
 
-         DB::beginTransaction();
-         try {
-         	DB::connection('follows')->table($follows_table)->insert([
-         		'uid' => $uid,
-         		'follow_uid' => $follow_uid,
-         		]);
-         	DB::connection('follows')->table($follows_me_table)->insert([
-         		'uid' => $uid,
-         		'follow_uid' => $follow_uid,
-         		]);
-            DB::commit();
-         } catch (Exception $e) {
-         	DB::rollback();
-         	return response()->json($e);
-         }
+         // DB::beginTransaction();
+         // try {
+         // 	DB::connection('follows')->table($follows_table)->insert([
+         // 		'uid' => $uid,
+         // 		'follow_uid' => $follow_uid,
+         // 		]);
+         // 	DB::connection('follows')->table($follows_me_table)->insert([
+         // 		'uid' => $uid,
+         // 		'follow_uid' => $follow_uid,
+         // 		]);
+         //    DB::commit();
+         // } catch (Exception $e) {
+         // 	DB::rollback();
+         // 	return response()->json($e);
+         // }
 
          Cache::increment(USER_FOLLOWS_COUNT . $uid);
          Cache::increment(USER_FOLLOWS_ME_COUNT . $follow_uid);

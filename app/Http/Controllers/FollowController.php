64d81ed4226e88ef56e9c9ab194a2685b783269d
echoLin <409086340@qq.com>
2016-09-13 23:26:03 +0800

    post follow queue

--- app/Http/Controllers/FollowController.php
+++ app/Http/Controllers/FollowController.php
@@ -32,24 +32,26 @@ class FollowController extends Controller
             return response()->json('您已关注' . MAX_FOLLOWS_COUNT . '人，不可再关注他人');
          }
 
-         $follows_table = getFollowsTable($uid);
-         $follows_me_table = getFollowsMeTable($follow_uid);
+         $this->dispatch(new PostFollow($uid, $follow_uid));
 
-         DB::beginTransaction();
-         try {
-         	DB::connection('follows')->table($follows_table)->insert([
-         		'uid' => $uid,
-         		'follow_uid' => $follow_uid,
-         		]);
-         	DB::connection('follows')->table($follows_me_table)->insert([
-         		'uid' => $uid,
-         		'follow_uid' => $follow_uid,
-         		]);
-            DB::commit();
-         } catch (Exception $e) {
-         	DB::rollback();
-         	return response()->json($e);
-         }
+         // $follows_table = getFollowsTable($uid);
+         // $follows_me_table = getFollowsMeTable($follow_uid);
+
+         // DB::beginTransaction();
+         // try {
+         // 	DB::connection('follows')->table($follows_table)->insert([
+         // 		'uid' => $uid,
+         // 		'follow_uid' => $follow_uid,
+         // 		]);
+         // 	DB::connection('follows')->table($follows_me_table)->insert([
+         // 		'uid' => $uid,
+         // 		'follow_uid' => $follow_uid,
+         // 		]);
+         //    DB::commit();
+         // } catch (Exception $e) {
+         // 	DB::rollback();
+         // 	return response()->json($e);
+         // }
 
          Cache::increment(USER_FOLLOWS_COUNT . $uid);
          Cache::increment(USER_FOLLOWS_ME_COUNT . $follow_uid);
---
+++ app/Jobs/PostFolllow.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Jobs;
+
+use DB;
+use App\Jobs\Job;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Redis as Redis;
+
+class PostFollow extends Job implements ShouldQueue
+{
+    use InteractsWithQueue, SerializesModels;
+
+    protected $uid;
+    protected $follow_uid;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($uid, $follow_uid)
+    {
+        $this->uid = $uid;
+        $this->follow_uid = $follow_uid;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+         $follows_table = getFollowsTable($this->uid);
+         $follows_me_table = getFollowsMeTable($this->follow_uid);
+
+         DB::beginTransaction();
+         try {
+            DB::connection('follows')->table($follows_table)->insert([
+                'uid' => $this->uid,
+                'follow_uid' => $this->follow_uid,
+                ]);
+            DB::connection('follows')->table($follows_me_table)->insert([
+                'uid' => $this->uid,
+                'follow_uid' => $this->follow_uid,
+                ]);
+            DB::commit();
+         } catch (Exception $e) {
+            DB::rollback();
+         }
+    }
+}
